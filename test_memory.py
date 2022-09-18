c1d5e2e4605dfa07aa47a0a33775c096d5f1d186
sahilghom003 <sahil.ghom@gslab.com>
2022-09-18 21:50:35 +0530

    sgdj

---
+++ test_memory.py
@@ -0,0 +1,49 @@
+import pytest
+import psutil
+import datetime
+import math
+
+memory = memory = psutil.virtual_memory()[2]
+print(memory)
+
+
+@pytest.mark.cli
+def test_virtual_memory():
+    # here taken virtual memory % is 90
+    if memory:
+        assert memory <= 90
+        print("Testcase passed")
+        data = "current virtual memory usage in % is:"
+        time = datetime.datetime.now()
+        details = data, memory, time
+        with open("pass_result.txt", "a") as f:
+            f.write("\n")
+            f.write(str(details))
+            f.close()
+    # else:
+    #     with open("pass_result.txt", "w") as f:
+    #         insufficient = "CPU usage is above 90%, testcase failed"
+    #         time = datetime.datetime.now()
+    #         details = insufficient, memory, time
+    #         f.write(str(details))
+    #         f.close()
+
+
+#
+cpu_usage = psutil.cpu_percent()
+print(cpu_usage)
+
+
+@pytest.mark.cli
+def test_cpu_usage():
+    # here taken cpu usage % is 90 for 4 secs
+    if cpu_usage:
+        assert cpu_usage <= 90
+        print("Testcase passed")
+        data = "current CPU usage in % is:"
+        time = datetime.datetime.now()
+        details = data, cpu_usage, time
+        with open("pass_result.txt", "a") as f:
+            f.write("\n")
+            f.write(str(details))
+            f.close()
